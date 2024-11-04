e832137a874242ebfc589ac60abe2a97e7b08705
phoebila <proyer@ucsc.edu>
2024-11-04 12:35:51 -0800

    fixing main

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 // todo
+const APP_NAME = "Demo 3 121";
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+document.title = APP_NAME;
+
+const heading = document.createElement("h1");
+heading.textContent = APP_NAME;
+
+const fakeButton = document.createElement("button");
+fakeButton.textContent = "Click me!";
+fakeButton.onclick = () => alert("You clicked me!");
+
+app.append(fakeButton);
